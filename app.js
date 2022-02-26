76516ab5b625935427ed8546fd0f8a190a9f238a
Mazed4D <playmyfiddleman@gmail.com>
2022-02-26 16:15:05 +0100

    added routes, packages, error handlers and middleware

--- app.js
+++ app.js
@@ -10,11 +10,17 @@ const app = express();
 
 // non-const imports
 require('dotenv').config();
+require('express-async-errors');
 
 // package imports
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+// security packages
+const helmet = require('helmet');
 const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 
 // other imports
 const connectDB = require('./database/connect');
@@ -22,6 +28,17 @@ const connectDB = require('./database/connect');
 // router imports
 const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts.js');
+
+// error handlers
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// security middleware
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimit({ windowsMs: 60 * 1000, max: 60 }));
 
 // middleware
 app.use(logger('dev'));
@@ -31,7 +48,12 @@ app.use(cookieParser());
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/posts', postsRouter);
+app.use('/api/v1/users', usersRouter);
+
+// error middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 // start func
 const start = async () => {
