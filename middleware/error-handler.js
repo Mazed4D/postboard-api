221fb2e21abd721691abaa4b712863e4bd3986a6
Mazed4D <playmyfiddleman@gmail.com>
2022-02-26 17:34:13 +0100

    modified error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -10,19 +10,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 	if (err.name === 'CastError') {
 		customError.msg = `No item found with id of ${err.value}`;
 		customError.statusCode = 404;
-	}
-
-	if ((err.name = 'ValidationError')) {
+	} else if ((err.name = 'ValidationError')) {
 		customError.msg = err.keyPattern;
 		customError.statusCode = 400;
-	}
-
-	if (err.code && err.code === 11000) {
+	} else if (err.code && err.code === 11000) {
 		customError.msg = `Duplicate value entered for ${Object.keys(
 			err.keyValue
 		)} field, please choose another value`;
 		customError.statusCode = 400;
 	}
+
 	return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
