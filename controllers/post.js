c0537b5b60c52418f6a45c07a4cf6da092fff9de
Mazed4D <playmyfiddleman@gmail.com>
2022-02-26 17:34:37 +0100

    added basic functionality to posts controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,17 +1,27 @@
+const Post = require('../models/Post');
+
 const addPost = async (req, res) => {
-	console.log('post route');
+	const post = new Post({
+		user: req.user.userId,
+		text: req.body.text,
+	});
+	const result = await post.save();
+	res.json(result);
 };
 
 const fetchPost = async (req, res) => {
-	console.log('fetch post');
+	const post = await Post.findById(req.params.id);
+	res.status(200).json(post);
 };
 
 const fetchPosts = async (req, res) => {
-	console.log('fetch posts');
+	const posts = await Post.find();
+	res.json(posts);
 };
 
 const fetchPostsByUser = async (req, res) => {
-	console.log('fetch posts by user');
+	const posts = await Post.find({ user: req.params.id });
+	res.status(200).json(posts);
 };
 
 module.exports = {
