13c8ad20258e780a21f9fefb072f4d59b13adb23
Mazed4D <playmyfiddleman@gmail.com>
2022-02-26 18:51:07 +0100

    basic functionality for liking implemented

--- controllers/likes.js
+++ controllers/likes.js
@@ -1,13 +1,40 @@
+const { StatusCodes } = require('http-status-codes');
+const Like = require('../models/Likes');
+
 const addLike = async (req, res) => {
-	res.send('add like');
+	const likeObj = {
+		post: req.params.id,
+		user: req.user.userId,
+	};
+	const isLike = await Like.findOne(likeObj);
+	if (isLike) {
+		return res.status(400).json({ msg: 'Already liked' });
+	}
+	const like = new Like(likeObj);
+	const result = await like.save();
+	res.status(200).json(result);
 };
 
 const removeLike = async (req, res) => {
-	res.send('remove like');
+	const foundLike = await Like.findById(req.params.id);
+	if (!foundLike) {
+		return res.status(404).json({ msg: 'Like not found' });
+	}
+	if (foundLike.user != req.user.userId) {
+		return res.status(StatusCodes.UNAUTHORIZED).json({ msg: 'Unauthorized' });
+	}
+	await Like.findByIdAndDelete(req.params.id);
+	res.status(200).json({ success: true, msg: 'like removed' });
 };
 
 const fetchLikes = async (req, res) => {
-	res.send('fetch likes');
+	const likes = await Like.find({ post: req.params.id });
+	if (!likes) {
+		return res
+			.status(404)
+			.json({ msg: `Post doesn't exist or it has no likes` });
+	}
+	res.status(200).json(likes);
 };
 
 module.exports = {
